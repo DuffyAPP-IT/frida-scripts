719d2ace3ab59a9f7b0fcb63b4b289acd30bda35
James Duffy <james@duffy.app>
2020-08-12 14:39:15 +0100

    nsurl-request addition

---
+++ nsurl-requests.js
@@ -0,0 +1,35 @@
+if (ObjC.available) {
+
+try {  
+
+var className = "NSURLRequest"; 
+var funcName = "- initWithURL:cachePolicy:timeoutInterval:"; 
+
+var hook = eval('ObjC.classes.' + className + '["' + funcName + '"]');
+
+Interceptor.attach(hook.implementation, {  
+   
+    onEnter: function(args) 
+    {  
+    console.log('URL -> ' + ObjC.Object(args[2]).toString() );
+    }, 
+  
+    onLeave: function(returnvalue) 
+    {  
+    }  
+ 
+    }); 
+   
+} 
+catch(error) 
+{
+ console.log("[!] Exception: " + error.message); 
+} 
+}  
+
+else { 
+
+console.log("Objective-C Runtime is not available!"); 
+
+}
+
